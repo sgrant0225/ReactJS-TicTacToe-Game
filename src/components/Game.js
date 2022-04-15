584adde7e2526fda99db899c082d619b3b31986b
github username <sharnellgrant@gmail.com>
2022-04-15 18:25:10 -0400

    Created the board and the sqaures component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import Board from './Board'
 
 export default function Game() {
-    return(
-        <div>
-            
+    const status = "Next player is X"
+    const moves = (
+        <li>
+            <button>Start the Game</button>
+        </li>
+    );
+    const squares = Array(9).fill(null) //creating an array of 9 elements and fill them with null values
+    return (
+       <div>
+           <div className="game-board">
+               <Board squares={squares}/>
+           </div>
+           <div className="game-info">
+             <div>{status}</div>
+             <ul>{moves}</ul>
+           </div>
         </div>
     )
 }
